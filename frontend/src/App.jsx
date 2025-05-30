d0a6acdab29033ad87334894ce4ea6fcfb5c8aa6
Costilla <lisbetcostilla@gmail.com>
2025-05-30 18:13:17 -0300

    password

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -61,8 +61,7 @@ function App() {
     <Route path="/forgot-password" element={<ForgotPassword />} />
     <Route path="/notfound" element={<NotFound />} />
     <Route path="/aboutme" element={<AboutMe />} />
-    <Route path="/resetpassword" element={<ResetPassword />} />
-
+    <Route path="/reset-password/:token" element={<ResetPassword />} />
   </Routes>
     <FloatingHelpButton />
     <Footer />
